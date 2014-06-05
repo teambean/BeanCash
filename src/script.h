6e131357291270210e624ff8473e0aa9a3637179
Wladimir J. van der Laan <laanwj@gmail.com>
2014-06-05 10:10:52 +0200

    Avoid undefined behavior using CFlatData in CScript serialization
    `&vch[vch.size()]` and even `&vch[0]` on vectors can cause assertion
    errors with VC in debug mode. This is the problem mentioned in #4239.
    The deeper problem with this is that we rely on undefined behavior.
    
    - Add `begin_ptr` and `end_ptr` functions that get the beginning and end
      pointer of vector in a reliable way that copes with empty vectors and
      doesn't reference outside the vector
    (see https://stackoverflow.com/questions/1339470/how-to-get-the-address-of-the-stdvector-buffer-start-most-elegantly/1339767#1339767).
    - Add a convenience constructor to CFlatData that wraps a vector.
    
    I added `begin_ptr` and `end_ptr` as separate functions as I imagine
    they will be useful in more places.

--- src/script.h
+++ src/script.h
@@ -589,10 +589,6 @@ public:
     }
 };
 
-
-
-
-
 bool EvalScript(std::vector<std::vector<unsigned char> >& stack, const CScript& script, const CTransaction& txTo, unsigned int nIn, int nHashType);
 bool Solver(const CScript& scriptPubKey, txnouttype& typeRet, std::vector<std::vector<unsigned char> >& vSolutionsRet);
 int ScriptSigArgsExpected(txnouttype t, const std::vector<std::vector<unsigned char> >& vSolutions);
