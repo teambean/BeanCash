f50aec325b54cfb53bf70c511043c9a90e86cd8a
Shawn Kent <admin@bitbean.org>
2018-10-31 14:26:37 -0400

    Refactored CheckBlockSignature()

--- src/main.cpp
+++ src/main.cpp
@@ -2374,15 +2374,13 @@ bool CBlock::SignBlock(CWallet& wallet, int64_t nFees)
 
 bool CBlock::CheckBlockSignature() const
 {
-    if (IsProofOfWork())
-        return vchBlockSig.empty();
+    if (vchBlockSig.empty())
+        return false;
 
-    vector<valtype> vSolutions;
     txnouttype whichType;
+    vector<valtype> vSolutions;
 
-    const CTxOut& txout = vtx[1].vout[1];
-
-    if (!Solver(txout.scriptPubKey, whichType, vSolutions))
+    if (!Solver(vtx[1].vout[1].scriptPubKey, whichType, vSolutions))
         return false;
 
     if (whichType == TX_PUBKEY)
@@ -2390,8 +2388,6 @@ bool CBlock::CheckBlockSignature() const
         valtype& vchPubKey = vSolutions[0];
         CKey key;
         if (!key.SetPubKey(vchPubKey))
-            return false;
-        if (vchBlockSig.empty())
             return false;
         return key.Verify(GetHash(), vchBlockSig);
     }
