6bd1639558f7c889add26997fa98ae2bc9622c7b
Shawn Kent <admin@bitbean.org>
2019-04-07 15:29:27 -0400

    Typo Fix

--- src/bitbeanrpc.h
+++ src/bitbeanrpc.h
@@ -69,7 +69,7 @@ enum RPCErrorCode
 json_spirit::Object JSONRPCError(int code, const std::string& message);
 
 void StartRPCThreads();
-void StopRPCThreadds();
+void StopRPCThreads();
 int CommandLineRPC(int argc, char *argv[]);
 
 /** Convert parameter values for RPC call from strings to command-specific JSON objects. */
