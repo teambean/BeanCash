30d9d5cbf44c8ba27ea0cbf36a5dcbbc7d2369b7
Shawn Kent <admin@bitbean.org>
2019-08-29 23:47:50 -0400

    Increase default outgoing connections

--- src/net.cpp
+++ src/net.cpp
@@ -17,7 +17,7 @@
 using namespace std;
 using namespace boost;
 
-static const int MAX_OUTBOUND_CONNECTIONS = 16;
+static const int MAX_OUTBOUND_CONNECTIONS = 32;
 
 bool OpenNetworkConnection(const CAddress& addrConnect, CSemaphoreGrant *grantOutbound = NULL, const char *strDest = NULL, bool fOneShot = false);
 
