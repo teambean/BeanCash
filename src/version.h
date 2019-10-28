16e771826a7a721b041c15ca9597a1febe3bf6c4
Shawn Kent <admin@bitbean.org>
2019-10-28 18:49:43 -0400

    Increase Protocol Version for v1.3 release

--- src/version.h
+++ src/version.h
@@ -31,7 +31,7 @@ static const int DATABASE_VERSION = 70509;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 60014;
+static const int PROTOCOL_VERSION = 60015;
 
 // intial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
@@ -44,8 +44,8 @@ static const int MIN_PEER_PROTO_VERSION = 209;
 static const int CADDR_TIME_VERSION = 31402;
 
 // only request blocks from nodes outside this range of versions
-static const int NOBLKS_VERSION_START = 60013;
-static const int NOBLKS_VERSION_END = 60014;
+static const int NOBLKS_VERSION_START = 60000;
+static const int NOBLKS_VERSION_END = 60013;
 
 // BIP 0031, pong message, is enabled for all versions AFTER this one
 static const int BIP0031_VERSION = 60000;
