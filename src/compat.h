f0f3dd86c3f83d0a1fa7d024e0dbcbdbbd787352
Shawn Kent <admin@bitbean.org>
2021-04-25 21:04:07 -0400

    Allow 4096 File Descriptors on Windows , prevent compiler redefinition warnings and cleanup compat.h Windows related

--- src/compat.h
+++ src/compat.h
@@ -3,10 +3,14 @@
 // Copyright (c) 2015-2021 Bean Core www.beancash.org
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 #ifndef _BITBEAN_COMPAT_H
-#define _BITBEAN_COMPAT_H 1
+#define _BITBEAN_COMPAT_H
 
 #ifdef WIN32
+#ifdef _WIN32_WINNT
+#undef _WIN32_WINNT
+#endif
 #define _WIN32_WINNT 0x0501
 #ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN 1
@@ -14,8 +18,11 @@
 #ifndef NOMINMAX
 #define NOMINMAX
 #endif
+#ifdef FD_SETSIZE
+#undef FD_SETSIZE // prevent redefinition compiler warning
+#endif
+#define FD_SETSIZE 4096 // Max number of File Descriptors for Windows
 #include <winsock2.h>
-#include <mswsock.h>
 #include <ws2tcpip.h>
 #else
 #include <sys/types.h>
@@ -24,19 +31,16 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 #include <net/if.h>
-#include <netinet/in.h>
-#include <ifaddrs.h>
+#include <netinet/in.h>  // Use sockaddr_in (POSIX.1-2001)
 #include <unistd.h>
-
-typedef u_int SOCKET;
+#include <ifaddrs.h>
 #endif
-
 
 #ifdef WIN32
 #define MSG_NOSIGNAL        0
 #define MSG_DONTWAIT        0
-typedef int socklen_t;
 #else
+typedef u_int SOCKET;
 #include "errno.h"
 #define WSAGetLastError()   errno
 #define WSAEINVAL           EINVAL
