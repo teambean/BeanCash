e4a2a8ede0158ff6243bd8402fd862df1370e1cf
Shawn Kent <admin@bitbean.org>
2021-05-03 18:09:15 -0400

    Don't treat fFromMe transactions differently when broadcasting

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -2188,20 +2188,6 @@ void CWallet::PrintWallet(const CBlock& block)
     LogPrintf("\n");
 }
 
-bool CWallet::GetTransaction(const uint256 &hashTx, CWalletTx& wtx)
-{
-    {
-        LOCK(cs_wallet);
-        map<uint256, CWalletTx>::iterator mi = mapWallet.find(hashTx);
-        if (mi != mapWallet.end())
-        {
-            wtx = (*mi).second;
-            return true;
-        }
-    }
-    return false;
-}
-
 bool CWallet::SetDefaultKey(const CPubKey &vchPubKey)
 {
     if (fFileBacked)
