59168f62a416990e970aad9dbdffa474d94f3bb1
Shawn Kent <admin@bitbean.org>
2021-05-07 19:59:35 -0400

    Revert "Changing LockedPageManager to use a managed instance"
    This reverts commit 3319d800bb7322f16a992bf019180648fa085848.

--- src/allocators.h
+++ src/allocators.h
@@ -9,7 +9,6 @@
 #include <string.h>
 #include <string>
 #include <boost/thread/mutex.hpp>
-#include <boost/thread/once.hpp>
 #include <map>
 #include <openssl/crypto.h>  // for OPENSSL_cleanse()
 
@@ -36,12 +35,6 @@ public:
         assert(!(page_size & (page_size-1))); // size must be power of two
         page_mask = ~(page_size - 1);
     }
-
-    ~LockedPageManagerBase()
-    {
-        assert(this->GetLockedPageCount() == 0);
-    }
-
 
     // For all pages in affected range, increase lock count
     void LockRange(void *p, size_t size)
@@ -126,39 +119,13 @@ public:
 /**
  * Singleton class to keep track of locked (ie, non-swappable) memory pages, for use in
  * std::allocator templates.
- *
- * Some implementations of the STL allocate memory in some constructors (i.e., see
- * MSVC's vector<T> implementation where it allocates 1 byte of memory in the allocator.)
- * Due to the unpredictable order of static initializers, we have to make sure the
- * LockedPageManager instance exists before any other STL-based objects that use
- * secure_allocator are created. So instead of having LockedPageManager also be
- * static-intialized, it is created on demand.
  */
 class LockedPageManager: public LockedPageManagerBase<MemoryPageLocker>
 {
 public:
-    static LockedPageManager& Instance() 
-    {
-        boost::call_once(LockedPageManager::CreateInstance, LockedPageManager::init_flag);
-        return *LockedPageManager::_instance;
-    }
-
+    static LockedPageManager instance; // instantiated in util.cpp
 private:
     LockedPageManager();
-
-    static void CreateInstance()
-    {
-        // Using a local static instance guarantees that the object is initialized
-        // when it's first needed and also deinitialized after all objects that use
-        // it are done with it.  I can think of one unlikely scenario where we may
-        // have a static deinitialization order/problem, but the check in
-        // LockedPageManagerBase's destructor helps us detect if that ever happens.
-        static LockedPageManager instance;
-        LockedPageManager::_instance = &instance;
-    }
-
-    static LockedPageManager* _instance;
-    static boost::once_flag init_flag;
 };
 
 //
@@ -166,12 +133,12 @@ private:
 // Intended for non-dynamically allocated structures.
 //
 template<typename T> void LockObject(const T &t) {
-    LockedPageManager::Instance().LockRange((void*)(&t), sizeof(T));
+    LockedPageManager::instance.LockRange((void*)(&t), sizeof(T));
 }
 
 template<typename T> void UnlockObject(const T &t) {
     OPENSSL_cleanse((void*)(&t), sizeof(T));
-    LockedPageManager::Instance().UnlockRange((void*)(&t), sizeof(T));
+    LockedPageManager::instance.UnlockRange((void*)(&t), sizeof(T));
 }
 
 //
@@ -203,7 +170,7 @@ struct secure_allocator : public std::allocator<T>
         T *p;
         p = std::allocator<T>::allocate(n, hint);
         if (p != NULL)
-            LockedPageManager::Instance().LockRange(p, sizeof(T) * n);
+            LockedPageManager::instance.LockRange(p, sizeof(T) * n);
         return p;
     }
 
@@ -212,7 +179,7 @@ struct secure_allocator : public std::allocator<T>
         if (p != NULL)
         {
             OPENSSL_cleanse(p, sizeof(T) * n);
-            LockedPageManager::Instance().UnlockRange(p, sizeof(T) * n);
+            LockedPageManager::instance.UnlockRange(p, sizeof(T) * n);
         }
         std::allocator<T>::deallocate(p, n);
     }
