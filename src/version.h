7f2596ff7fbd3b6b1b6455c7a3b500d3d9127138
Shawn Kent <admin@bitbean.org>
2021-08-17 14:17:28 -0400

    Increase Proto version for 1.3.2.1

--- src/version.h
+++ src/version.h
@@ -35,10 +35,10 @@ static const int DATABASE_VERSION = 70509;
 static const int PROTOCOL_VERSION = 60015;
 
 // intial proto version, to be increased after version/verack negotiation
-static const int INIT_PROTO_VERSION = 210;
+static const int INIT_PROTO_VERSION = 211;
 
 // disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 209;
+static const int MIN_PEER_PROTO_VERSION = 210;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
